383331e322fdd6fe528e212c31a64c949c183fc8
Atenas Arita <a00838500@tec.mx>
2024-12-28 12:22:18 -0600

    New intro transitions added.
    intro to the website transition added, as well as a cleaner transition for the about section

--- script.js
+++ script.js
@@ -6,6 +6,40 @@ function toggleMenu() {
 }
 
 
+const intro = document.querySelector('.intro');
+const logo = document.querySelector('.logo-header');
+const logoSpans = document.querySelectorAll('.logo-1, .logo-2');
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(fadeInLogoSpans, 200);
+});
+
+function fadeInLogoSpans() {
+    logoSpans.forEach((span) => span.classList.add('fade-in'));
+
+    setTimeout(() => {
+        logoSpans.forEach((span) => span.classList.add('active'));
+    }, 1000);
+
+    setTimeout(() => {
+        logoSpans.forEach((span) => span.classList.add('reposition'));
+      }, 1500);    
+
+    setTimeout(fadeOutIntro, 1600); // After 1.5s, the logo will fade out
+}
+
+function fadeOutIntro() {
+    intro.classList.add('out');
+
+    setTimeout(removeIntro, 1000); // Wait for the intro to fully wipe off
+}
+
+function removeIntro() {
+    intro.remove(); // Remove the entire intro from the DOM
+    logo.remove();
+}
+
+
 let section1Items = document.querySelectorAll('.about-pic-container, .section_text');
 window.addEventListener('scroll', () => {
     let scrollPosition = window.scrollY;
@@ -19,23 +53,4 @@ window.addEventListener('scroll', () => {
                 section1Items.classList.add('show-animate');
         }
     });
-});
-
-
-let bannerContainer = document.querySelector('#banner-section'); 
-
-window.addEventListener('scroll', () => {
-    let scrollPosition = window.scrollY; // Get the current scroll position
-    let containerTop = bannerContainer.offsetTop; // Get position of banner
-    let containerHeight = bannerContainer.offsetHeight; // Height of the banner
-
-    // How far the banner is from the top, relative to the viewport
-    let distanceFromTop = scrollPosition - containerTop + window.innerHeight / 0.9;
-
-    // Normalize the scroll distance to a value between 0 and 1
-    let scrollPercent = Math.min(Math.max(distanceFromTop / containerHeight, 0), 1);
-
-    // Scale the banner (base scale of 1, grows to 1.2 as you scroll)
-    let scaleValue = 1 + scrollPercent * 0.2; // Adjust 0.2 to control max growth
-    bannerContainer.style.transform = `scale(${scaleValue})`;
 });
